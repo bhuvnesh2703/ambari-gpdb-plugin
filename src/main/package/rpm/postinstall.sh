31570d571e753d00f7dcee075aaab11ceb68cc5d
Mahesh Kumar <mvasanthu@pivotal.io>
2015-02-05 13:40:23 -0800

    [#87653734] Updating postinstall script

--- src/main/package/rpm/postinstall.sh
+++ src/main/package/rpm/postinstall.sh
@@ -5,15 +5,15 @@ import json
 from pprint import pprint
 json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.1/role_command_order.json', 'r+')
 data = json.load(json_data)
-data['general_deps']['HAWQ-INSTALL'] = "[HDFS-INSTALL]"
-data['general_deps']['HAWQMASTER-START'] = "[NAMENODE-START]"
+data['general_deps']['HAWQ-INSTALL'] = ['HDFS-INSTALL']
+data['general_deps']['HAWQMASTER-START'] = ['NAMENODE-START']
 json_data.seek(0)
 json.dump(data, json_data, indent=2)
 json_data.close()
 json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.2/role_command_order.json', 'r+')
 data = json.load(json_data)
-data['general_deps']['HAWQ-INSTALL'] = "[HDFS-INSTALL]"
-data['general_deps']['HAWQMASTER-START'] = "[NAMENODE-START]"
+data['general_deps']['HAWQ-INSTALL'] = ['HDFS-INSTALL']
+data['general_deps']['HAWQMASTER-START'] = ['NAMENODE-START']
 json_data.seek(0)
 json.dump(data, json_data, indent=2)
 json_data.close()
