2277c83797699a8da6908253ca8bbd2d2d8faf6a
Mahesh Kumar <mvasanthu@pivotal.io>
2015-02-23 16:09:33 -0800

    [#88959552] Updating PHD version as per ambari-1.7.1

--- src/main/package/rpm/postinstall.sh
+++ src/main/package/rpm/postinstall.sh
@@ -3,14 +3,7 @@
 python <<EOT
 import json
 from pprint import pprint
-json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.1/role_command_order.json', 'r+')
-data = json.load(json_data)
-data['general_deps']['HAWQ-INSTALL'] = ['HDFS-INSTALL']
-data['general_deps']['HAWQMASTER-START'] = ['NAMENODE-START']
-json_data.seek(0)
-json.dump(data, json_data, indent=2)
-json_data.close()
-json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.2/role_command_order.json', 'r+')
+json_data=open('/var/lib/ambari-server/resources/stacks/PHD/3.0/role_command_order.json', 'r+')
 data = json.load(json_data)
 data['general_deps']['HAWQ-INSTALL'] = ['HDFS-INSTALL']
 data['general_deps']['HAWQMASTER-START'] = ['NAMENODE-START']
--- src/main/package/rpm/preremove.sh
+++ src/main/package/rpm/preremove.sh
@@ -3,7 +3,7 @@
 python <<EOT
 import json
 from pprint import pprint
-json_data=open('/var/lib/ambari-server/resources/stacks/PHD/3.0.0.0/role_command_order.json', 'r+')
+json_data=open('/var/lib/ambari-server/resources/stacks/PHD/3.0/role_command_order.json', 'r+')
 data = json.load(json_data)
 json_data.close()
 EOT
