92d6240493babe1d1d302167f90d4e7291c221d1
Mahesh Vasanthu <mvasanthu@office-4-95.pa.gopivotal.com>
2015-02-25 10:58:54 -0800

    [#88959552] Correcting postinstall and preremove scripts logic

--- src/main/package/rpm/postinstall.sh
+++ src/main/package/rpm/postinstall.sh
@@ -5,7 +5,7 @@ import json
 import os
 from pprint import pprint
 
-if os.path.exists('/var/lib/ambari-server/resources/stacks/PHD/3.0'):
+if os.path.exists('/var/lib/ambari-server/resources/stacks/PHD/3.0/role_command_order.json'):
   json_data=open('/var/lib/ambari-server/resources/stacks/PHD/3.0/role_command_order.json', 'r+')
   data = json.load(json_data)
   data['general_deps']['HAWQ-INSTALL'] = ['HDFS-INSTALL']
@@ -13,7 +13,7 @@ if os.path.exists('/var/lib/ambari-server/resources/stacks/PHD/3.0'):
   json_data.seek(0)
   json.dump(data, json_data, indent=2)
   json_data.close()
-elif os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.2'):
+elif os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.2/role_command_order.json'):
   json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.1/role_command_order.json', 'r+')
   data = json.load(json_data)
   data['general_deps']['HAWQ-INSTALL'] = ['HDFS-INSTALL']
--- src/main/package/rpm/preremove.sh
+++ src/main/package/rpm/preremove.sh
@@ -5,8 +5,8 @@ import json
 import os
 from pprint import pprint
 
-if os.path.exists('/var/lib/ambari-server/resources/stacks/PHD/3.0'):
-  json_data=open('/var/lib/ambari-server/resources/stacks/PHD/3.0', 'r+')
+if os.path.exists('/var/lib/ambari-server/resources/stacks/PHD/3.0/role_command_order.json'):
+  json_data=open('/var/lib/ambari-server/resources/stacks/PHD/3.0/role_command_order.json', 'r+')
   data = json.load(json_data)
   if data['general_deps'].has_key('HAWQ-INSTALL'):
     data['general_deps'].pop('HAWQ-INSTALL')
@@ -16,7 +16,7 @@ if os.path.exists('/var/lib/ambari-server/resources/stacks/PHD/3.0'):
   json_data.truncate()
   json.dump(data, json_data, indent=2)
   json_data.close()
-elif os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.2'):
+elif os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.2/role_command_order.json'):
   json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.1/role_command_order.json', 'r+')
   data = json.load(json_data)
   if data['general_deps'].has_key('HAWQ-INSTALL'):
