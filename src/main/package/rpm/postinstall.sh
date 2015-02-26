ef878ddc0dede9ec4cbd36b16c8ca0d897bfee28
maheshkumarvs <mvasanthu@pivotal.io>
2015-02-26 10:35:13 -0800

    [#89215068] Removed hawq-plugin support for HDP-2.1

--- src/main/package/rpm/postinstall.sh
+++ src/main/package/rpm/postinstall.sh
@@ -14,13 +14,6 @@ if os.path.exists('/var/lib/ambari-server/resources/stacks/PHD/3.0/role_command_
   json.dump(data, json_data, indent=2)
   json_data.close()
 elif os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.2/role_command_order.json'):
-  json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.1/role_command_order.json', 'r+')
-  data = json.load(json_data)
-  data['general_deps']['HAWQ-INSTALL'] = ['HDFS-INSTALL']
-  data['general_deps']['HAWQMASTER-START'] = ['NAMENODE-START']
-  json_data.seek(0)
-  json.dump(data, json_data, indent=2)
-  json_data.close()
   json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.2/role_command_order.json', 'r+')
   data = json.load(json_data)
   data['general_deps']['HAWQ-INSTALL'] = ['HDFS-INSTALL']
--- src/main/package/rpm/preremove.sh
+++ src/main/package/rpm/preremove.sh
@@ -17,16 +17,6 @@ if os.path.exists('/var/lib/ambari-server/resources/stacks/PHD/3.0/role_command_
   json.dump(data, json_data, indent=2)
   json_data.close()
 elif os.path.exists('/var/lib/ambari-server/resources/stacks/HDP/2.2/role_command_order.json'):
-  json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.1/role_command_order.json', 'r+')
-  data = json.load(json_data)
-  if data['general_deps'].has_key('HAWQ-INSTALL'):
-    data['general_deps'].pop('HAWQ-INSTALL')
-  if data['general_deps'].has_key('HAWQMASTER-START'):
-    data['general_deps'].pop('HAWQMASTER-START')
-  json_data.seek(0)
-  json_data.truncate()
-  json.dump(data, json_data, indent=2)
-  json_data.close()
   json_data=open('/var/lib/ambari-server/resources/stacks/HDP/2.2/role_command_order.json', 'r+')
   data = json.load(json_data)
   if data['general_deps'].has_key('HAWQ-INSTALL'):
