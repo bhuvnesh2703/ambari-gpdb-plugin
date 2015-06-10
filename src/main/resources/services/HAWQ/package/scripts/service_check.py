bbd7f0c26eaf5ce3a7a2c1bf655c590f2050ff3a
Newton Alex <nalex@pivotal.io>
2015-06-09 18:53:45 -0700

    Fixed the smoke tests source command issue

--- src/main/resources/services/HAWQ/package/scripts/service_check.py
+++ src/main/resources/services/HAWQ/package/scripts/service_check.py
@@ -6,50 +6,60 @@ class HAWQServiceCheck(Script):
   sqlCommand = None
   remote_hostname = None
 
-  def __init__():
-    this.sqlCommand = "su -c \"source /usr/local/hawq/greenplum_path.sh && psql -c '{0};'\" gpadmin";
-    this.remote_hostname = params.hawq_master
+  def __init__(self):
+    self.sqlCommand = "su gpadmin -c \\\"source /usr/local/hawq/greenplum_path.sh && psql -c '{0};'\\\""
 
   def service_check(self, env):
     hawq.verify_segments_state(env)
 
-    drop_table()
-    create_table()
-    insert_data()
-    query_data()
-    cleanup()
+    import params
+    self.remote_hostname = params.hawq_master
+
+    self.drop_table()
+    self.create_table()
+    self.insert_data()
+    self.query_data()
+    self.cleanup()
 
 
-  def drop_table():
-    command = this.sqlCommand.format("drop table if exists ambari_smoke_t1")
-    (retcode, out, err) = subprocess_command_with_results(command, this.remote_hostname)
+  def drop_table(self):
+    print "Testing Drop Table"
+    command = self.sqlCommand.format("drop table if exists ambari_smoke_t1")
+    (retcode, out, err) = subprocess_command_with_results(command, self.remote_hostname)
     if retcode:
       raise Exception("HAWQ Smoke Tests Failed. Return Code: {0}. Out: {1} Error: {2}".format(retcode, out, err))
+    print out
 
     
-  def create_table():
-    command = this.sqlCommand.format("create table ambari_smoke_t1(a int)")
-    (retcode, out, err) = subprocess_command_with_results(command, this.remote_hostname)
+  def create_table(self):
+    print "Testing Create Table"
+    command = self.sqlCommand.format("create table ambari_smoke_t1(a int)")
+    (retcode, out, err) = subprocess_command_with_results(command, self.remote_hostname)
     if retcode:
       raise Exception("HAWQ Smoke Tests Failed. Return Code: {0}. Out: {1} Error: {2}".format(retcode, out, err))
+    print out
 
 
-  def insert_data():
-    command = this.sqlCommand.format("insert into ambari_smoke_t1 select * from generate_series(1,10)")
-    (retcode, out, err) = subprocess_command_with_results(command, this.remote_hostname)
+  def insert_data(self):
+    print "Testing Insert Data"
+    command = self.sqlCommand.format("insert into ambari_smoke_t1 select * from generate_series(1,10)")
+    (retcode, out, err) = subprocess_command_with_results(command, self.remote_hostname)
     if retcode:
       raise Exception("HAWQ Smoke Tests Failed. Return Code: {0}. Out: {1} Error: {2}".format(retcode, out, err))
+    print out
 
 
-  def query_data():
-    command = this.sqlCommand.format("select * from ambari_smoke_t1")
-    (retcode, out, err) = subprocess_command_with_results(command, this.remote_hostname)
+  def query_data(self):
+    print "Testing Querying Data"
+    command = self.sqlCommand.format("select * from ambari_smoke_t1")
+    (retcode, out, err) = subprocess_command_with_results(command, self.remote_hostname)
     if retcode:
       raise Exception("HAWQ Smoke Tests Failed. Return Code: {0}. Out: {1} Error: {2}".format(retcode, out, err))
+    print out
 
 
-  def cleanup():
-    drop_table()
+  def cleanup(self):
+    self.drop_table()
 
 
 if __name__ == "__main__":
