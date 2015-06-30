b6ca3f55597dbb3b2d66ce61dbb19238348d5102
Bhuvnesh Chaudhary <bhuvnesh2703@gmail.com>
2015-06-30 10:12:27 -0700

    [#97844242] - Fixed code review comments

--- src/main/resources/services/HAWQ/package/scripts/common.py
+++ src/main/resources/services/HAWQ/package/scripts/common.py
@@ -1,7 +1,5 @@
 import os
 import subprocess
-import re
-from resource_management.core.logger import Logger
 
 def subprocess_command_with_results(cmd, remote_hostname=None):
   if remote_hostname:
@@ -13,14 +11,3 @@ def subprocess_command_with_results(cmd, remote_hostname=None):
                              shell=True)
   (stdoutdata, stderrdata) = process.communicate()
   return process.returncode, stdoutdata, stderrdata
-
-def get_processes_on_port(port):
-  # Identify processes running on input port
-  cmd = "netstat -tupln | egrep ':{0}'".format(port)
-  (retcode, out, err) = subprocess_command_with_results(cmd)
-  if err:
-    raise Exception("Execution of command `%s` failed. Error returned is \n%s" % (cmd, err))
-  # Strip multiple spaces in the output to single space, and remove the newline character from output
-  out = re.sub(" +", " ", out).strip()
-  Logger.info("List of process running on port %d:\n%s" % (port, out.split('\n') if len(out) > 0 else 'None' ))
-  return out
--- src/main/resources/services/HAWQ/package/scripts/hawq.py
+++ src/main/resources/services/HAWQ/package/scripts/hawq.py
@@ -1,7 +1,7 @@
 from resource_management import *
 from Hardware import Hardware
 from verifications import Verifications
-from common import subprocess_command_with_results, get_processes_on_port
+from common import subprocess_command_with_results
 import specs
 import os
 import subprocess
@@ -336,17 +336,15 @@ def master_start(env=None):
     command = source + cmd
     Execute(command, user=params.hawq_user, timeout=600)
 
-def is_hawq_running():
-  import params
-  processes = get_processes_on_port(params.hawq_master_port)
-  # Split the columns in the output, return True if the process running on master port contains postgres else False
-  return any('postgres' in process for process in processes.split())
-
 def master_stop(env=None):
   import params
-  if is_hawq_running():
-    cmd = "source /usr/local/hawq/greenplum_path.sh; gpstop -af -d {0}/gpseg-1".format(params.hawq_master_dir)
-    Execute(cmd, user=params.hawq_user, timeout=600)
+  command = "source /usr/local/hawq/greenplum_path.sh; gpstop -af -d {0}/gpseg-1".format(params.hawq_master_dir)
+  # If postgres process is running on hawq master port, gpstop should be executed otherwise it should be skipped. Note: {0}\s captures processes running on port 5432 and not on 5432[0-9]
+  hawq_process_chk_cmd = "if [ `(netstat -tupln | egrep ':{0}\s' | egrep postgres | awk 'END {{print FNR}}')` -gt 0 ] ; then (exit 1) ; else (exit 0) ; fi".format(params.hawq_master_port)
+  Execute(command,
+          user=params.hawq_user, 
+          timeout=600, 
+          not_if="if [ `(netstat -tupln | egrep ':{0}\s' | egrep postgres | awk 'END {{print FNR}}')` -gt 0 ] ; then (exit 1) ; else (exit 0) ; fi".format(params.hawq_master_port))
 
 def metrics_start(env=None):
   import params
